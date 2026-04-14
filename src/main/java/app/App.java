512219cb162406cb13bd5c24fe07b368604c844c
jjguillen <jjavierguillen@gmail.com>
2026-04-14 17:10:42 +0200

    ConsultasVenta

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,6 +1,7 @@
 package app;
 
 import entidades.Venta;
+import servicios.ConsultasVenta;
 import utilidades.CsvLoader;
 
 import java.util.List;
@@ -11,7 +12,30 @@ public class App {
 
         //Cargar fichero ventas.csv
         List<Venta> ventas = CsvLoader.cargarVentas();
-        ventas.forEach(System.out::println);
+        //ventas.forEach(System.out::println);
+
+        //Crear servicio ConsultasVenta
+        ConsultasVenta consVenta = new ConsultasVenta(ventas);
+
+        //Hacer consultas
+        IO.println("--- 1. ---> getVentasMayor100");
+        IO.println(consVenta.getVentasMayor100().stream().count());
+
+        IO.println("--- 2. ---> getVentasByCategoriaElectronica");
+        consVenta.getVentasByCategoriaElectronica().forEach(System.out::println);
+
+        IO.println("--- 3. ---> getProductosVendidosOrder");
+        consVenta.getProductosVendidosOrder().forEach(System.out::println);
+
+        IO.println("--- 4. ---> getPrimeraVentaSpain");
+        consVenta.getPrimeraVentaSpain().ifPresent(System.out::println);
+
+        IO.println("--- 5. ---> getTop10VentasPorImporteTotal()");
+        consVenta.getTop10VentasPorImporteTotal().forEach(System.out::println);
+
+
+
+
 
 
     }
--- src/main/java/servicios/ConsultasVenta.java
+++ src/main/java/servicios/ConsultasVenta.java
@@ -3,7 +3,11 @@ package servicios;
 import entidades.Venta;
 import lombok.Data;
 
+import java.util.Comparator;
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Data
 public class ConsultasVenta {
@@ -46,6 +50,52 @@ public class ConsultasVenta {
                 .sorted()
                 .toList();
     }
+
+    /**
+     * Devuelve la primera Venta en España
+     * @return Optional<Venta>
+     */
+    public Optional<Venta> getPrimeraVentaSpain() {
+        return ventas.stream()
+                .filter(v -> v.getPais().equals("Spain"))
+                .findFirst();
+    }
+
+    /**
+     * Devuelve las 10 ventas con mayor importe total
+     * @return
+     */
+    public List<Venta> getTop10VentasPorImporteTotal() {
+        return ventas.stream()
+                .sorted(Comparator.comparing(Venta::getTotalLinea).reversed())
+                .limit(10)
+                .toList();
+    }
+
+    /**
+     * Devuelve el importe total de las ventas
+     * @return
+     */
+    public double getFacturacionTotal() {
+        return ventas.stream()
+                .mapToDouble(Venta::getTotalLinea)
+                .sum();
+    }
+
+    /**
+     * Estadísticas de precio unitario
+     * @return
+     */
+    public DoubleSummaryStatistics getEstadisticasPrecioUnitario() {
+        //return ventas.stream()
+        //        .collect(Collectors.summarizingDouble(Venta::getPrecioUnitario));
+
+        return ventas.stream()
+                .mapToDouble(Venta::getPrecioUnitario)
+                .summaryStatistics();
+    }
+
+
 
 
 
